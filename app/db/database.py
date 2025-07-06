401d4691552aecf7a4a072b29c816559ca9e3769
maokami <zerozerozero0216@gmail.com>
2025-07-06 12:30:22 +0900

    fix: Correct database connection logic for PostgreSQL

--- app/db/database.py
+++ app/db/database.py
@@ -22,9 +22,13 @@ if SQLALCHEMY_DATABASE_URL.startswith(
 
 logger.debug(f"Connecting to database at: {SQLALCHEMY_DATABASE_URL}")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
